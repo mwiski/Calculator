ba12c551b3e5c0f4fa2bf224349d1e0181089b8e
mwiski <49036916+mwiski@users.noreply.github.com>
2019-03-31 16:41:20 +0200

    Kod 4.6.a (#1)
    * KOD-4.6.a: multiplication and division methods
    
    * KOD-4.6.a: space before else
    
    * KOD-4.6.a: added throw in method division

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -10,10 +10,24 @@ public class Calculator {
         return c - d;
     }
 
+    public double multiplication (double e, double f) {
+        return e * f;
+    }
+
+    public double division (double g, double h) {
+        if (h != 0) {
+            return g / h;
+        } else {
+            throw new IllegalArgumentException("Dont dive by zero");
+        }
+    }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
         System.out.println(calculator.addition(5,5));
         System.out.println(calculator.subtraction(10,5));
+        System.out.println(calculator.multiplication(5,5));
+        System.out.println(calculator.division(20,5));
     }
 }
