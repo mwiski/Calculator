6b94e5756ce06556e6cb32d2794377f347a554e3
mwiski <49036916+mwiski@users.noreply.github.com>
2019-04-04 16:08:02 +0200

    fibonacci (#2)
    * fibonacci
    
    * fibonacci: changing method1
    
    * fibonacci: changing method2
    
    * fibonacci change n1 = 1 instead of 0

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -10,16 +10,33 @@ public class Calculator {
         return c - d;
     }
 
-    public double multiplication (double e, double f) {
+    public double multiplication(double e, double f) {
         return e * f;
     }
 
-    public double division (double g, double h) {
+    public double division(double g, double h) {
         if (h != 0) {
             return g / h;
         } else {
             throw new IllegalArgumentException("Dont dive by zero");
         }
+    }
+
+    public double fibonacci(double n) {
+        if (n == 1 || n == 0)
+            return 1;
+
+        if (n < 0)
+            throw new IllegalArgumentException("Number must be 0 or higher.");
+
+        double n1 = 1, n2 = 1, n3 = 0;
+        for (int i = 1; i < n; i++){
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+
+        return n3;
     }
 
     public static void main(String[] args) {
@@ -29,5 +46,6 @@ public class Calculator {
         System.out.println(calculator.subtraction(10,5));
         System.out.println(calculator.multiplication(5,5));
         System.out.println(calculator.division(20,5));
+        System.out.println(calculator.fibonacci(1000));
     }
 }
